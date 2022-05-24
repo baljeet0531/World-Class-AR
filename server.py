b9b0924ce35d14a9d7def2e7825206cc58328534
baljeet0531 <barryfang0531@gmail.com>
2022-05-24 12:42:20 +0800

    add tequila html

--- server.py
+++ server.py
@@ -4,4 +4,4 @@ app = Flask(__name__, static_folder='/')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run("localhost", 8000, debug=True)
